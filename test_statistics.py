e93c6b3cf01311d13ca78054f4c30519a2bdb000
Pong <115053913+Pong50887@users.noreply.github.com>
2024-09-16 15:31:28 +0700

    Update test_statistics.py

--- test_statistics.py
+++ test_statistics.py
@@ -1,23 +1,25 @@
 from unittest import TestCase
-from statistics import variance, stdev
+from statistics import variance, stdev, average
 from math import sqrt
+
 
 class StatisticsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
     
     def test_variance_non_integers(self):
         """variance should work with decimal values"""
         # variance([x,y,z]) == variance([x+d,y+d,z+d]) for any d
         self.assertAlmostEqual(4.0, variance([0.1, 4.1]))
-        # variance([0,4,4,8]) == 8
+        # variance([0, 4, 4, 8]) == 8
         self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]))
 
     def test_stdev(self):
+        """Test standard deviation function"""
         # standard deviation of a single value should be zero
         self.assertEqual(0.0, stdev([10.0]))
         # simple test
@@ -26,7 +28,13 @@ class StatisticsTest(TestCase):
         self.assertEqual(sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]))
 
 
-if __name__ == '__main__':
-    import unittest
-    unittest.main(verbosity=1)
+class EmptyTest(TestCase):
+    def test_variance_empty_list(self):
+        """variance should raise ValueError for empty list"""
+        with self.assertRaises(ValueError):
+            variance([])
 
+    def test_average_empty_list(self):
+        """average should raise ValueError for empty list"""
+        with self.assertRaises(ValueError):
+            average([])
